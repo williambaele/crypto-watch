ae7bfdb02b2990ea1020c1e687182b26a776952c
William Baele <baelewilliam1999@gmail.com>
2023-03-30 16:47:58 +0200

    Added table components

--- src/components/HeaderLeftTop.js
+++ src/components/HeaderLeftTop.js
@@ -6,11 +6,20 @@ const HeaderLeftTop = () => {
 
   const [headerData, setHeaderData] = useState([]);
 
-  useEffect(() => {
+  const fetchData = () => {
     axios
       .get("https://api.coingecko.com/api/v3/global")
-      .then((res) => setHeaderData(res.data.data))
-  }, [])
+      .then((res) => setHeaderData(res.data.data));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleReloadClick = () => {
+    fetchData();
+    console.log("reloded");
+  };
 
   return (
     <div className="md:col-span-2 md:row-span-4 bg-[#121420] rounded-md shadow-sm grid p-2 px-4 relative">
@@ -39,7 +48,7 @@ const HeaderLeftTop = () => {
           {headerData.market_cap_percentage &&
             headerData.market_cap_percentage.eth.toFixed(2)+ "%"}
         </p>
-      <div className="absolute top-2 right-2 p-2 cursor-pointer">
+      <div className="absolute top-2 right-2 p-2 cursor-pointer reload" onClick={handleReloadClick}>
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#9CA3AF" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
           <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
