5b0d68fb715f531329840106046a073d68d97872
William Baele <baelewilliam1999@gmail.com>
2023-03-30 10:25:47 +0200

    Added fonts & some style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
 
 function App() {
   return (
-  <div className="h-screen bg-[#14213d]">
-      <p>Hi</p>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+    </Routes>
+  </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="h-screen bg-[#14213D] py-10">
+      <div className="container mx-auto">
+        <h1>Hi</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
