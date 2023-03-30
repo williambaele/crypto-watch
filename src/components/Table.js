81bad8f9cfd9132ac8f41919329d8905e54cc528
William Baele <baelewilliam1999@gmail.com>
2023-03-30 16:58:38 +0200

    Added table components

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,125 +1,130 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Table = ({coinsData}) => {
   console.log(coinsData);
+
+
+  const [rangeNumber, setRangeNumber] = useState(100);
+
+
   return (
 
-<div class="relative overflow-x-auto rounded-md">
-    <table class="w-full text-sm text-left text-gray-500">
-        <thead class="text-xs uppercase bg-[#121420]">
+<div className="relative overflow-x-auto rounded-md">
+    <table className="w-full text-sm text-left text-gray-500">
+        <thead class="text-sm uppercase bg-[#121420]">
             <tr>
-              <th scope="col" class="px-6 py-3">
-                Top
+              <th scope="col" className="px-2 py-3 flex items-center">
+                Top <input className="bg-[#121420] pl-2 text-sm text-gray-500 uppercase w-1/3 outline-none" type="text" value={rangeNumber} onChange={(e) => setRangeNumber(e.target.value)} />
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 XXXX
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Prix
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 MarketCap
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Volume
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 1h
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 1j
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 1s
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 1m
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 6m
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 1y
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 ATH
               </th>
             </tr>
         </thead>
         <tbody>
-          <tr class="bg-none border-b border-[#6B7280]">
-              <th scope="row" class="px-6 py-4 font-medium">
+          <tr className="bg-none border-b border-[#6B7280]">
+              <th scope="row" className="px-6 py-4 font-medium">
                 1
               </th>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 Silver
               </td>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 Laptop
               </td>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 $2999
               </td>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 $2999
               </td>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 $2999
               </td>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 $2999
               </td>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 $2999
               </td>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 $2999
               </td>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 $2999
               </td>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 $2999
               </td>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 $2999
               </td>
           </tr>
-          <tr class="bg-none border-b border-[#6B7280]">
-              <th scope="row" class="px-6 py-4 font-medium">
+          <tr className="bg-none border-b border-[#6B7280]">
+              <th scope="row" className="px-6 py-4 font-medium">
                 1
               </th>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 Silver
               </td>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 Laptop
               </td>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 $2999
               </td>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 $2999
               </td>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 $2999
               </td>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 $2999
               </td>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 $2999
               </td>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 $2999
               </td>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 $2999
               </td>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 $2999
               </td>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 $2999
               </td>
           </tr>
